f465c89ff24d0a82919be35c388946443915efba
nimraash <nimra3538216@gmail.com>
2025-04-20 16:22:19 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+name = input("Enter the girl name:")
+programming_language = input("Enter a programming language (e.g: python. next.js . typescript . html): ")
+mentor = input("Enter the mentor name:")
+
+print("\n Here is my little story based on mad libs game!")
+print(f"once upon a time, there was a girl was a girl named {name}.")
+print(f"{name} was very curious and always want to learn new things.")
+print(f"one day, she decided to learn {programming_language} language at governor house!")
+print(f"luckily {name} found a great mentor name as {mentor} who was an expert in {programming_language}.")
+print("And so, her journey began.......!")
